2be7c2318541b1010a677c17a6373b34adb4c290
StructuredTrading <samowen1992@hotmail.com>
2024-05-25 22:45:19 +1000

    feat: Added sketch pad to game page

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,21 @@
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+
+export default function Canvas() {
+
+    const styles = {
+        border: '0.0625rem solid #9c9c9c',
+        borderRadius: '0.25rem',
+      };
+
+    return (
+        <>
+            <ReactSketchCanvas
+                style={styles}
+                width="600"
+                height="400"
+                strokeWidth={4}
+                strokeColor="red"
+            />
+        </>
+    )
+};
