ca9f44f6b3f28444b9830ac9103f7233708b1ee5
lushepherd <143170142+lushepherd@users.noreply.github.com>
2024-05-26 17:26:52 +1000

    feat(style): add styles for AboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,16 @@
-
+import { Link } from "react-router-dom";
+import Logo from '../components/Logo.jsx';
+import '../styles/AboutPage.css'; 
 
 export default function AboutPage() {
 
-    return (
-        <>
-
-        </>
-    )
+    return <>
+        <div class="page-container">
+            <Logo />
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus natus aliquam in cupiditate, fuga harum eaque aperiam sapiente ducimus soluta provident repellat quaerat minus at veniam minima? Provident laboriosam id culpa consequuntur fugiat, beatae corporis tempora odio nesciunt, repellat, similique esse delectus possimus nam aliquid rerum. Eum nemo expedita quisquam.</p>
+            <Link to={"/"}>Go home</Link>
+        </div>
+    </>
+    
 }
